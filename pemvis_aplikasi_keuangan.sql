69c72e964cc5927429052450c128422b1bff163f
syamil09 <56720985+syamil09@users.noreply.github.com>
2026-01-14 20:03:15 +0700

    update database

--- pemvis_aplikasi_keuangan.sql
+++ pemvis_aplikasi_keuangan.sql
@@ -648,6 +648,9 @@ ALTER TABLE `transaction_categories`
   ADD CONSTRAINT `transaction_categories_ibfk_2` FOREIGN KEY (`credit_account_id`) REFERENCES `accounts` (`account_id`);
 COMMIT;
 
+ALTER TABLE journal_entries 
+MODIFY COLUMN transaction_type ENUM('invoice','expense','receipt','adjustment','payment','payroll') NOT NULL;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
